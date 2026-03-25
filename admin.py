ece9726397286941cd14c4c7ec46c65f17c01306
maddipie056 <maddireese105@gmail.com>
2026-03-25 14:02:08 -0400

    Update admin.py

--- admin.py
+++ admin.py
@@ -7,3 +7,21 @@ def dashboard():
     if 'loggedin' not in session or session.get('role') != 'admin':
         return redirect(url_for('auth.login'))
     return render_template('admin/dashboard.html', username=session['username'])
+
+@admin.route('/view_accounts')
+def view_accounts():
+    if 'loggedin' not in session or session.get('role') != 'admin':
+        return redirect(url_for('auth.login'))
+    return render_template('admin/view_accounts.html', username=session['username'])
+
+@admin.route('/create_account')
+def create_account():
+    if 'loggedin' not in session or session.get('role') != 'admin':
+        return redirect(url_for('auth.login'))
+    return render_template('admin/create_account.html', username=session['username'])
+
+@admin.route('/roles')
+def roles():
+    if 'loggedin' not in session or session.get('role') != 'admin':
+        return redirect(url_for('auth.login'))
+    return render_template('admin/roles.html', username=session['username'])
