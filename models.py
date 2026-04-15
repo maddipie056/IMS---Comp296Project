00751e194613dfaa061d8fc42734303a16a13e7d
maddipie056 <maddireese105@gmail.com>
2026-04-15 13:57:53 -0400

    Update models.py

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from app import db
+from apps import db
 
 class Item(db.Model):
     __table__ = db.Model.metadata.tables['items']
@@ -8,3 +8,6 @@ class Staff(db.Model):
 
 class StockAdjustment(db.Model):
     __table__ = db.Model.metadata.tables['stock_adjustments']
+
+class Category(db.Model):
+    __table__ = db.Model.metadata.tables['categories']
